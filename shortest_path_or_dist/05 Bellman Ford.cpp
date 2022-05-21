8dc3531aec5cead1beeb1c4a0611317b5e643f7c
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-22 01:38:41 +0530

    Update 05 Bellman Ford.cpp

--- shortest_path_or_dist/05 Bellman Ford.cpp
+++ shortest_path_or_dist/05 Bellman Ford.cpp
@@ -2,6 +2,23 @@
 
 // time complexity of Bellman-Ford is O(VE), which is more than Dijkstra. Only advantage over Dijkstra is the ability to handle -ive weights.
 
+
+//SUPER IMP: it works for -ive weights, BUT NOT for -ive weight CYCLES
+
+
+// Algorithm 
+// Input: Graph and a source vertex src 
+// Output: Shortest distance to all vertices from src. If there is a negative weight cycle, 
+//         then shortest distances are not calculated, negative weight cycle is reported.
+// 1) This step initializes distances from the source to all vertices as infinite and distance to the source itself as 0. 
+//    Create an array dist[] of size |V| with all values as infinite except dist[src] where src is source vertex.
+// 2) This step calculates shortest distances. Do following |V|-1 times where |V| is the number of vertices in given graph. 
+// …..a) Do following for each edge u-v 
+// ………………If dist[v] > dist[u] + weight of edge uv, then update dist[v] 
+// ………………….dist[v] = dist[u] + weight of edge uv
+// 3) This step reports if there is a negative weight cycle in graph. Do following for each edge u-v 
+// ……If dist[v] > dist[u] + weight of edge uv, then “Graph contains negative weight cycle” 
+
 #include <bits/stdc++.h> 
 
 struct Edge { 
