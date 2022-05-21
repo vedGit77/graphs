814a4c33b26beaedd44df4a953ad26298f7e532e
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-21 16:28:49 +0530

    Update 03 Minimum spanning tree_Prims Algo.cpp

--- shortest_path_or_dist/03 Minimum spanning tree_Prims Algo.cpp
+++ shortest_path_or_dist/03 Minimum spanning tree_Prims Algo.cpp
@@ -25,7 +25,7 @@ int primMST(int graph[V][V])
 { 
 
 	int res=0; 
-	bool mSet[V]; 
+	bool mstSet[V]; 
 	int key[V];
 	fill(key,key+V,INT_MAX);
 	key[0]=0;
@@ -35,14 +35,14 @@ int primMST(int graph[V][V])
 		int u = -1; 
 
 		for(int i=0;i<V;i++)
-		    if(!mSet[i]&&(u==-1||key[i]<key[u]))
+		    if(!mstSet[i] && (u==-1||key[i]<key[u]))
 		        u=i;
-		mSet[u] = true; 
+		
+		mstSet[u] = true; 
 		res+=key[u];
 
-		
 		for (int v = 0; v < V; v++) 
-			if (graph[u][v]!=0 && mSet[v] == false) 
+			if (graph[u][v]!=0 && mstSet[v] == false) 
 				key[v] = min(key[v],graph[u][v]); 
 	} 
     return res;
