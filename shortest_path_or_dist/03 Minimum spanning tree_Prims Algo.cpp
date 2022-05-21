ca6a9eabe080a5d68b681bdc4425e15552db852f
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-21 16:24:46 +0530

    Update 03 Minimum spanning tree_Prims Algo.cpp

--- shortest_path_or_dist/03 Minimum spanning tree_Prims Algo.cpp
+++ shortest_path_or_dist/03 Minimum spanning tree_Prims Algo.cpp
@@ -7,6 +7,16 @@
 // How many edges does a minimum spanning tree has? 
 // A minimum spanning tree has (V – 1) edges where V is the number of vertices in the given graph. 
 
+// Algorithm 
+// 1) Create a set mstSet that keeps track of vertices already included in MST. 
+// 2) Assign a key value to all vertices in the input graph. Initialize all key values as INFINITE. 
+// 	Assign key value as 0 for the first vertex so that it is picked first. 
+// 3) While mstSet doesn’t include all vertices 
+// ….a) Pick a vertex u which is not there in mstSet and has minimum key value. 
+// ….b) Include u to mstSet. 
+// ….c) Update key value of all adjacent vertices of u. To update the key values, iterate through all adjacent vertices. 
+// 	For every adjacent vertex v, if weight of edge u-v is less than the previous key value of v, update the key value as weight of u-v
+
 #include <bits/stdc++.h> 
 using namespace std; 
 #define V 4
