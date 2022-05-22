c89bdbe94eac2d70ec7d17f59dbbbb1beb7fc25c
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-23 01:16:14 +0530

    Update 02 Kruskal's Algorithm.cpp

--- Minimum spanning Tree/02 Kruskal's Algorithm.cpp
+++ Minimum spanning Tree/02 Kruskal's Algorithm.cpp
@@ -1,3 +1,9 @@
+// Below are the steps for finding MST using Kruskal’s algorithm
+// 1. Sort all the edges in non-decreasing order of their weight. 
+// 2. Pick the smallest edge. Check if it forms a cycle with the spanning tree formed so far. If cycle is not formed, include this edge. Else, discard it. 
+// 3. Repeat step#2 until there are (V-1) edges in the spanning tree.
+
+
 // C++ program for Kruskal's algorithm to find Minimum Spanning Tree 
 // of a given connected, undirected and weighted graph 
 #include <bits/stdc++.h> 
