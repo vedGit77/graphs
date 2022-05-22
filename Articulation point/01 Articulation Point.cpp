aa1a478d45a2c54b6f1e871bb7b20e71d60a573b
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-22 11:30:46 +0530

    Update 01 Articulation Point.cpp

--- Articulation point/01 Articulation Point.cpp
+++ Articulation point/01 Articulation Point.cpp
@@ -13,14 +13,30 @@
 // Time complexity of above method is O(V*(V+E)) for a graph
 
 
-// Efficient soln:
+
+
+// Efficient soln: (Time complexity is same as DFS which is O(V+E) for adjacency list representation of graph)
+
 // The idea is to use DFS (Depth First Search). In DFS, we follow vertices in tree form called DFS tree. 
 // In DFS tree, a vertex u is parent of another vertex v, if v is discovered by u (obviously v is an adjacent of u in graph).
 // In DFS tree, a vertex u is articulation point if one of the following two conditions is true. 
 // 1) u is root of DFS tree and it has at least two children. 
 // 2) u is not root of DFS tree and it has a child v such that NO vertex in subtree 
 //    rooted with v has a back edge to one of the ancestors (in DFS tree) of u.
+//------------------------------------------Explaining the efficient Approach--------------------------------------------
+
+// We do DFS traversal of given graph with additional code to find out Articulation Points (APs). 
+// In DFS traversal, we maintain a parent[] array where parent[u] stores parent of vertex u. 
+// Among the above mentioned two cases, the first case is simple to detect. 
+// For every vertex, count children. If currently visited vertex u is root (parent[u] is NIL) and has more than two children, print it. 
+
+// How to handle second case? The second case is trickier. We maintain an array disc[] to store discovery time of vertices. 
+// For every node u, we need to find out the earliest visited vertex (the vertex with minimum discovery time) that can be reached from subtree rooted with u. 
+// So we maintain an additional array low[] which is defined as follows:
+// 	low[u] = min(disc[u], disc[w]) 
+// where w is an ancestor of u and there is a back edge from some descendant of u to w.
 	
+
 #include<iostream> 
 #include <list> 
 #define NIL -1
