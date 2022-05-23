63e613aadf5e443107d46502ca0e7174e0c4c55a
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-23 15:41:58 +0530

    Update 03 Shortest Path Algorithm_Dijkstra's_(directedORundirected)+cyclic+weighted.cpp

--- shortest_path_or_dist/03 Shortest Path Algorithm_Dijkstra's_(directedORundirected)+cyclic+weighted.cpp
+++ shortest_path_or_dist/03 Shortest Path Algorithm_Dijkstra's_(directedORundirected)+cyclic+weighted.cpp
@@ -37,7 +37,7 @@ vector<int> djikstra(int graph[V][V],int src)
 		int u = -1; 			     //and there we used to print the sum of the array elements
 
 		for(int i=0;i<V;i++)
-		    if(!fin[i]&&(u==-1||dist[i]<dist[u]))
+		    if( !fin[i] && (u==-1||dist[i]<dist[u]) )
 		        u=i;
 		
 		fin[u] = true; 
