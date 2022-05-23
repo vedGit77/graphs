c47f350b1cd9b443055f8be1cb966a2bb53847b9
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-23 15:46:37 +0530

    Update 01 Minimum spanning tree_Prims Algo.cpp

--- Minimum spanning Tree/01 Minimum spanning tree_Prims Algo.cpp
+++ Minimum spanning Tree/01 Minimum spanning tree_Prims Algo.cpp
@@ -43,8 +43,8 @@ int primMST(int graph[V][V])
 
 		for (int v = 0; v < V; v++) //step 3c.//Update key value of all adjacent vertices of u.
 			if (graph[u][v]!=0 && mstSet[v] == false) 
-				key[v] = min(key[v],graph[u][v]); 
-	} 
+				key[v] = min(key[v],graph[u][v]); //in Dijsktras, it was min(key[v],key[u]+graph[u][v])
+	} 							  //since there we had to find shortest distance
     return res;
 } 
 
