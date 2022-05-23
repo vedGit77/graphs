7be7e7f92d3703be3372250a30c542e69922bc19
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-23 12:30:47 +0530

    Update 01 Articulation Point.cpp

--- Articulation point/01 Articulation Point.cpp
+++ Articulation point/01 Articulation Point.cpp
@@ -98,7 +98,8 @@ void Graph::APUtil(int u, bool visited[], int disc[], int low[], int parent[], b
 						//thus we need ATLEAST 1 NON back edge to prove articulation point (parent MUST be non root)
 		
 		else if (v != parent[u])    //keep updating the low[] array values if already visited that node (and that node is NOT parent)
-			low[u] = min(low[u], disc[v]); //NOT min(low[u],low[v])....since see the defn of low[] again...or see the photo attached
+			low[u] = min(low[u], disc[v]); //NOT min(low[u],low[v])...ye karoge then low[] will be 1 for all vertices
+							// see the defn of low[] again...or see the photo attached
 	} 
 } 
 
