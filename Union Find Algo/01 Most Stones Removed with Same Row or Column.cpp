2768e05abb9b80a69d54f60e4afe4961c3ad32bd
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-26 18:16:48 +0530

    Update 01 Most Stones Removed with Same Row or Column.cpp

--- Union Find Algo/01 Most Stones Removed with Same Row or Column.cpp
+++ Union Find Algo/01 Most Stones Removed with Same Row or Column.cpp
@@ -101,9 +101,12 @@
 // has O(N) Time, O(N) Space
 
 
- int removeStones(vector<vector<int>>& stones) {
+class Solution {
+public:
+     int removeStones(vector<vector<int>>& stones) {
         for (int i = 0; i < stones.size(); ++i)
             uni(stones[i][0], ~stones[i][1]);
+         
         return stones.size() - islands;
     }
 
@@ -111,12 +114,24 @@
     int islands = 0;
 
     int find(int x) {
-        if (!f.count(x)) f[x] = x, islands++;
-        if (x != f[x]) f[x] = find(f[x]);
+        if (!f.count(x))
+        {
+            f[x] = x; 
+            islands++;
+        }
+        if (x != f[x]) 
+            f[x] = find(f[x]);
+        
         return f[x];
     }
 
     void uni(int x, int y) {
-        x = find(x), y = find(y);
-        if (x != y) f[x] = y, islands--;
+        x = find(x);
+        y = find(y);
+        if (x != y) 
+        {
+            f[x] = y;
+            islands--;
+        }
     }
+};
