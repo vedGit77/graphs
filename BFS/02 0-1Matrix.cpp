04995f76988e3d5f4f71c42aadd405ff3f82d4e5
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-05-31 16:49:59 +0530

    Update and rename 02 0-1_Matrix.cpp to 02 0-1Matrix.cpp

---
+++ BFS/02 0-1Matrix.cpp
@@ -0,0 +1,70 @@
+//Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+//The distance between two adjacent cells is 1.
+
+// ques link:
+// https://leetcode.com/problems/01-matrix/
+
+
+// Approach-1:
+
+//shortest paths-unweighted graph---->bfs(very standard)
+//but usually 1 source node---minimum distance from that particular node
+//but here multiple source nodes
+//so push all of them first
+//WHY???
+//WHEN WE POP AND EXPLORE WE ARE DOING SO IN INCREASING ORDER OF THEIR LEVELS
+//SO WHEN WE DISCOVER A NODE FIRST WE ARE EXPLORING IT FROM MINI POSSIBLE LEVEL NODE THAT IT IS CONNECTED TO
+//first the source..then level 1 nodes..then level 2 and so on
+//here the case is there are many level 0 nodes
+//so push all of them first!!!
+
+
+// Approach-2 :check 43rd file in dp repo
+
+class Solution {
+public:
+    bool isvalid(int i,int j,int n,int m)
+    {
+        if(i==n||j==m||j<0||i<0)
+            return false;
+        return true;
+    }
+    
+    vector<vector<int>> direction={{1,0},{0,1},{0,-1},{-1,0}};
+    
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) 
+    {
+        queue<pair<int,int>> q;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,-1));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                { 
+                    q.push({i,j});
+                    ans[i][j]=0;
+                }
+            }
+        }
+        
+        while(!q.empty())
+        {
+            pair<int,int> curr=q.front();
+            q.pop();
+            for(auto& x:direction)
+            {
+                int a=curr.first+x[0];
+                int b=curr.second+x[1];
+                if(isvalid(a,b,n,m) && ans[a][b]==-1)
+                {
+                    q.push({a,b}); //moving to the next levels in bfs....
+                    ans[a][b]=ans[curr.first][curr.second]+1;
+                }
+            }
+        }
+        return ans;
+    }
+};
