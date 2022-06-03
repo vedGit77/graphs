2fe2085b16e6b7c2e6bd43ff9ad4e81ca68b963c
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-03 13:17:16 +0530

    Update 04 Dijkstra's using Priority Queue.cpp

--- shortest_path_or_dist/04 Dijkstra's using Priority Queue.cpp
+++ shortest_path_or_dist/04 Dijkstra's using Priority Queue.cpp
@@ -5,19 +5,45 @@ using namespace std;
 // iPair ==> Integer Pair
 typedef pair<int, int> iPair;
 
-// To add an edge
-void addEdge(vector <pair<int, int> > adj[], int u,int v, int wt)
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
 {
-	adj[u].push_back(make_pair(v, wt));
-	adj[v].push_back(make_pair(u, wt));
+	int V; // No. of vertices
+
+	// In a weighted graph, we need to store vertex
+	// and weight pair for every edge
+	list< pair<int, int> > *adj;
+
+public:
+	Graph(int V); // Constructor
+
+	// function to add an edge to graph
+	void addEdge(int u, int v, int w);
+
+	// prints shortest path from s
+	void shortestPath(int s);
+};
+
+// Allocates memory for adjacency list
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<iPair> [V];
 }
 
+void Graph::addEdge(int u, int v, int w)
+{
+	adj[u].push_back(make_pair(v, w));
+	adj[v].push_back(make_pair(u, w));
+}
 
 // Prints shortest paths from src to all other vertices
-void shortestPath(vector<pair<int,int> > adj[], int V, int src)
+void Graph::shortestPath(int src)
 {
-	// Create a priority queue to store vertices that are being preprocessed.
-	priority_queue< iPair, vector <iPair> , greater<iPair> > pq; //NOTE: priority_queue mei if we want MIN heap, ALWAYS THIS WIERD SYNTAX....vector<iPair> in the middle
+	// Create a priority queue to store vertices that
+	// are being preprocessed. 
+	priority_queue< iPair, vector <iPair> , greater<iPair> > pq;//NOTE: priority_queue mei if we want MIN heap, ALWAYS THIS WIERD SYNTAX....vector<iPair> in the middle
 
 	// Create a vector for distances and initialize all
 	// distances as infinite (INF)
@@ -32,20 +58,22 @@ void shortestPath(vector<pair<int,int> > adj[], int V, int src)
 	distances are not finalized) */
 	while (!pq.empty())
 	{
-		// The first vertex in pair is the minimum distance vertex, extract it from priority queue.
+		// The first vertex in pair is the minimum distance
+		// vertex, extract it from priority queue.
 		// vertex label is stored in second of pair (it
 		// has to be done this way to keep the vertices
 		// sorted distance (distance must be first item
 		// in pair)
-		int u = pq.top().second;
+		int u = pq.top().second;//pq.top().first always stores the minimum distance of vertex x from the source vertex, and pq.top().second stores the vertex no. x
 		pq.pop();
 
-		// Get all adjacent of u.
-		for (auto x : adj[u])
+		// 'i' is used to get all adjacent vertices of a vertex
+		list< pair<int, int> >::iterator i;
+		for (i = adj[u].begin(); i != adj[u].end(); ++i)
 		{
 			// Get vertex label and weight of current adjacent of u.
-			int v = x.first;
-			int weight = x.second;
+			int v = (*i).first;
+			int weight = (*i).second;
 
 			// If there is shorted path to v through u.
 			if (dist[v] > dist[u] + weight)
@@ -63,27 +91,31 @@ void shortestPath(vector<pair<int,int> > adj[], int V, int src)
 		printf("%d \t\t %d\n", i, dist[i]);
 }
 
+// Driver program to test methods of graph class
 int main()
 {
+	// create the graph given in above figure
 	int V = 9;
-	vector<iPair > adj[V];
+	Graph g(V);
 
-	addEdge(adj, 0, 1, 4);
-	addEdge(adj, 0, 7, 8);
-	addEdge(adj, 1, 2, 8);
-	addEdge(adj, 1, 7, 11);
-	addEdge(adj, 2, 3, 7);
-	addEdge(adj, 2, 8, 2);
-	addEdge(adj, 2, 5, 4);
-	addEdge(adj, 3, 4, 9);
-	addEdge(adj, 3, 5, 14);
-	addEdge(adj, 4, 5, 10);
-	addEdge(adj, 5, 6, 2);
-	addEdge(adj, 6, 7, 1);
-	addEdge(adj, 6, 8, 6);
-	addEdge(adj, 7, 8, 7);
+	// making above shown graph
+	g.addEdge(0, 1, 4);
+	g.addEdge(0, 7, 8);
+	g.addEdge(1, 2, 8);
+	g.addEdge(1, 7, 11);
+	g.addEdge(2, 3, 7);
+	g.addEdge(2, 8, 2);
+	g.addEdge(2, 5, 4);
+	g.addEdge(3, 4, 9);
+	g.addEdge(3, 5, 14);
+	g.addEdge(4, 5, 10);
+	g.addEdge(5, 6, 2);
+	g.addEdge(6, 7, 1);
+	g.addEdge(6, 8, 6);
+	g.addEdge(7, 8, 7);
 
-	shortestPath(adj, V, 0);
+	g.shortestPath(0);
 
 	return 0;
 }
+	
