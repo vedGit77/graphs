667bfbdf1e661d2b1651d4fca13b7135124da8df
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-01 14:52:54 +0530

    Update 06 Floyd Warshall Algorithm.cpp

--- shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
+++ shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
@@ -30,8 +30,19 @@
 
 
 
+// Floyd Warshall Algorithm 
+// 1. We initialize the solution matrix same as the input graph matrix as a first step.
+// 2.Then we update the solution matrix by considering all vertices as an intermediate vertex. 
+//   The idea is to one by one pick all vertices and update all shortest paths which include the picked vertex as an intermediate vertex in the shortest path. 
+//   When we pick vertex number k as an intermediate vertex, we already have considered vertices {0, 1, 2, .. k-1} as intermediate vertices. 
+//   For every pair (i, j) of the source and destination vertices respectively, there are two possible cases:
+// 
+// a) k is not an intermediate vertex in shortest path from i to j. We keep the value of dist[i][j] as it is. 
+// b) k is an intermediate vertex in shortest path from i to j. 
+//    We update the value of dist[i][j] as dist[i][k] + dist[k][j] if dist[i][j] > dist[i][k] + dist[k][j]
+
 	
-// C++ Program for Floyd Warshall Algorithm
+
 #include <bits/stdc++.h>
 using namespace std;
 
