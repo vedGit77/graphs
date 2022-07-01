df4097364ad81ecd159ad983362bcb48f900185a
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-01 15:16:16 +0530

    Update 06 Floyd Warshall Algorithm.cpp

--- shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
+++ shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
@@ -46,67 +46,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Number of vertices in the graph
 #define V 4
-
-/* Define Infinite as a large enough
-value.This value will be used for
-vertices not connected to each other */
 #define INF 99999
-
-// A function to print the solution matrix
-void printSolution(int dist[][V]);
-
-// Solves the all-pairs shortest path
-// problem using Floyd Warshall algorithm
-void floydWarshall(int graph[][V])
-{
-	/* dist[][] will be the output matrix
-	that will finally have the shortest
-	distances between every pair of vertices */
-	int dist[V][V], i, j, k;
-
-	/* Initialize the solution matrix same
-	as input graph matrix. Or we can say
-	the initial values of shortest distances
-	are based on shortest paths considering
-	no intermediate vertex. */
-	for (i = 0; i < V; i++)
-		for (j = 0; j < V; j++)
-			dist[i][j] = graph[i][j];
-
-	/* Add all vertices one by one to
-	the set of intermediate vertices.
-	---> Before start of an iteration,
-	we have shortest distances between all
-	pairs of vertices such that the
-	shortest distances consider only the
-	vertices in set {0, 1, 2, .. k-1} as
-	intermediate vertices.
-	----> After the end of an iteration,
-	vertex no. k is added to the set of
-	intermediate vertices and the set becomes {0, 1, 2, ..
-	k} */
-	for (k = 0; k < V; k++) {
-		// Pick all vertices as source one by one
-		for (i = 0; i < V; i++) {
-			// Pick all vertices as destination for the
-			// above picked source
-			for (j = 0; j < V; j++) {
-				// If vertex k is on the shortest path from
-				// i to j, then update the value of
-				// dist[i][j]
-				if (dist[i][j] > (dist[i][k] + dist[k][j])
-					&& (dist[k][j] != INF
-						&& dist[i][k] != INF))
-					dist[i][j] = dist[i][k] + dist[k][j];
-			}
-		}
-	}
-
-	// Print the shortest distance matrix
-	printSolution(dist);
-}
 
 /* A utility function to print solution */
 void printSolution(int dist[][V])
@@ -124,6 +65,37 @@ void printSolution(int dist[][V])
 		}
 		cout << endl;
 	}
+}
+
+// Solves the all-pairs shortest path problem using Floyd Warshall algorithm
+void floydWarshall(int graph[][V])
+{
+	int dist[V][V]; //this will be our final ans
+	int i, j, k;
+
+	for (i = 0; i < V; i++) // Initialize the solution matrix same as input graph matrix. 
+		for (j = 0; j < V; j++)
+			dist[i][j] = graph[i][j];
+
+	/*Before start of an iteration, we have shortest distances between all
+	pairs of vertices such that the shortest distances consider only the
+	vertices in set {0, 1, 2, .. k-1} as intermediate vertices.*/
+	
+	for (k = 0; k < V; k++) //start with OUTER LOOP OF K
+	{  
+		for (i = 0; i < V; i++)  // Pick all vertices as source one by one
+		{
+			for (j = 0; j < V; j++) // Pick all vertices as destination for the above picked source
+			{
+				// If vertex k is on the shortest path from i to j, then update the value of dist[i][j]
+				if ( dist[i][j] > (dist[i][k] + dist[k][j]) //condition-1
+				    && (dist[k][j] != INF && dist[i][k] != INF)) //condition-2
+					dist[i][j] = dist[i][k] + dist[k][j];
+			}
+		}
+	}
+
+	printSolution(dist); //print the shortest dist
 }
 
 // Driver code
