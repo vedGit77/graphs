d34709dc58d9c323dacb921677a9cff06e029ebc
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-01 15:17:41 +0530

    Update 06 Floyd Warshall Algorithm.cpp

--- shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
+++ shortest_path_or_dist/06 Floyd Warshall Algorithm.cpp
@@ -104,16 +104,16 @@ int main()
 	 /* Let us create the following weighted graph
             10
     (0)------->(3)
-        |     /|\
-    5 |     |
-        |     | 1
-    \|/     |
+     |       /|\
+   5 |        |
+     |        | 1
+    \|/       |
     (1)------->(2)
             3     */
 	int graph[V][V] = { { 0, 5, INF, 10 },
-						{ INF, 0, 3, INF },
-						{ INF, INF, 0, 1 },
-						{ INF, INF, INF, 0 } };
+			{ INF, 0, 3, INF },
+			{ INF, INF, 0, 1 },
+			{ INF, INF, INF, 0 } };
 
 	// Print the solution
 	floydWarshall(graph);
