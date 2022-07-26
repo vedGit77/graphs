236f57d16af4b4d1da2e2b9ae8b5294847ae2e85
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-26 12:52:47 +0530

    Update 00 **IMP LCA** SEE FAST! .cpp

--- 00 **IMP LCA** SEE FAST! .cpp
+++ 00 **IMP LCA** SEE FAST! .cpp
@@ -56,20 +56,25 @@ public:
 //method 2
 
 class Solution {
- public:
-     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-         if(root==NULL) return NULL;
-    
-         if(root==p || root==q) return root;
-    
-         TreeNode* left=lowestCommonAncestor(root->left,p,q);
-         TreeNode* right=lowestCommonAncestor(root->right,p,q);
-    
-         if(left!=NULL and right!=NULL) return root;
-    
-         if(left==NULL and right==NULL) return NULL;
-    
-         return left!=NULL? left : right;
-     }
- };
-
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return NULL;
+        
+        if(root==p || root==q)
+            return root;
+        
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        
+        if(l!=NULL && r!=NULL)
+            return root;   //imp
+        
+        if(r!=NULL)
+            return r;     //imp...not root
+        if(l!=NULL)
+            return l;    //imp...not root
+        
+        return NULL;
+    }
+};
