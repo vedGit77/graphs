fe6eb82508bc8f31646009b15028eaca686df762
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-26 12:44:30 +0530

    Create 00 **IMP** SEE FAST! .cpp

---
+++ 00 **IMP** SEE FAST! .cpp
@@ -0,0 +1,75 @@
+// Binary tree -> 2 nodes given ->find their LCA
+
+//method 2 is better 
+
+
+//method 1 -> make an array of paths from root->p1 and root->p2...then using those paths, find the last element from the start which is present in both of them
+
+class Solution {
+public:
+    int flag=0;
+    void dfs(TreeNode* root, TreeNode* x, vector<TreeNode*>&v)
+    {
+        if(root==NULL || flag==1)
+            return;
+        
+        v.push_back(root);
+        
+        if(root==x)
+        {
+            flag=1;
+            return;
+        }
+        
+        if(root->left && flag==0)
+            dfs(root->left,x,v);
+        
+        if(root->right && flag==0)
+            dfs(root->right,x,v);
+        
+        if(flag==0)
+            v.pop_back();
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*>path1,path2;
+        
+        flag=0;
+        dfs(root,p,path1);
+        flag=0;
+        dfs(root,q,path2);        
+        
+        int count = -1;
+        for(int i=0; i<path1.size() && i<path2.size(); i++)
+        {
+            if(path1[i] == path2[i]) 
+                count++;
+            else 
+                break;
+        }
+                
+        return count==-1 ? NULL : path1[count];
+    }
+};
+
+
+
+//method 2
+
+class Solution {
+ public:
+     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+         if(root==NULL) return NULL;
+    
+         if(root==p || root==q) return root;
+    
+         TreeNode* left=lowestCommonAncestor(root->left,p,q);
+         TreeNode* right=lowestCommonAncestor(root->right,p,q);
+    
+         if(left!=NULL and right!=NULL) return root;
+    
+         if(left==NULL and right==NULL) return NULL;
+    
+         return left!=NULL? left : right;
+     }
+ };
+
