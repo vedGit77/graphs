66d2f922eded1ee8deb65052581b512d30957826
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-10 16:12:43 +0530

    Update 02 0-1_BFS.cpp

--- BFS/02 0-1_BFS.cpp
+++ BFS/02 0-1_BFS.cpp
@@ -8,6 +8,7 @@
   
 // We use DEQUEUE to store the node. 
 // if a edge having weight = 0 => node is pushed at front of DEQUEUE, and if a edge having weight = 1 => pushed at back of DEQUEUE.
+// so that vertices are processed in increasing order of weights.
   
 // The approach is similar to Dijkstra => if the shortest distance to node is relaxed by the previous node => then only it will be pushed in the queue. 
 
@@ -32,69 +33,39 @@ struct node
 // vector to store edges
 vector <node> edges[V];
 
-// Prints shortest distance from given source to every other verte
+
 void zeroOneBFS(int src)
 {
-// Initialize distances from given source
-  int dist[V];
-  for (int i=0; i<V; i++)
-  dist[i] = INT_MAX;
+	// Initialize distances from given source
+	int dist[V];
+	for (int i=0; i<V; i++)
+		dist[i] = INT_MAX;
 
-  // double ende queue to do BFS.
-  deque <int> Q;
-  dist[src] = 0;
-  Q.push_back(src);
+	// double ende queue to do BFS.
+	deque <int> Q;
+	dist[src] = 0;
+	Q.push_back(src);
 
-  while (!Q.empty())
-  {
-    int v = Q.front();
-    Q.pop_front();
+	while (!Q.empty())
+	{
+		int v = Q.front();
+		Q.pop_front();
 
-    for (int i=0; i<edges[v].size(); i++)
-    {
-      // checking for the optimal distance
-      if (dist[edges[v][i].to] > dist[v] + edges[v][i].weight)
-      {
-        dist[edges[v][i].to] = dist[v] + edges[v][i].weight;
+		for (int i=0; i<edges[v].size(); i++)
+		{
+			// checking for the optimal distance
+			if (dist[edges[v][i].to] > dist[v] + edges[v][i].weight)
+			{
+				dist[edges[v][i].to] = dist[v] + edges[v][i].weight;
 
-        // Put 0 weight edges to front and 1 weight
-        // edges to back so that vertices are processed
-        // in increasing order of weights.
-        if (edges[v][i].weight == 0)
-          Q.push_front(edges[v][i].to);
-        else
-          Q.push_back(edges[v][i].to);
-      }
-    }
-  }
+				if (edges[v][i].weight == 0)
+					Q.push_front(edges[v][i].to);  //push_front
+				else
+					Q.push_back(edges[v][i].to);   //push_back
+			}
+		}
+	}
 
-  for (int i=0; i<V; i++) // printing the shortest distances
-    cout << dist[i] << " ";
-}
-
-void addEdge(int u, int v, int wt)
-{
-edges[u].push_back({v, wt});
-edges[v].push_back({u, wt});
-}
-
-// Driver function
-int main()
-{
-	addEdge(0, 1, 0);
-	addEdge(0, 7, 1);
-	addEdge(1, 7, 1);
-	addEdge(1, 2, 1);
-	addEdge(2, 3, 0);
-	addEdge(2, 5, 0);
-	addEdge(2, 8, 1);
-	addEdge(3, 4, 1);
-	addEdge(3, 5, 1);
-	addEdge(4, 5, 1);
-	addEdge(5, 6, 1);
-	addEdge(6, 7, 1);
-	addEdge(7, 8, 1);
-	int src = 0;//source node
-	zeroOneBFS(src);
-	return 0;
+	for (int i=0; i<V; i++) // printing the shortest distances
+		cout << dist[i] << " ";
 }
