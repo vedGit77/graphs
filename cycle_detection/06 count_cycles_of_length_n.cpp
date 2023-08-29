1f11f61ebd6044adf164c673bfe97aa9984b0132
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-08-29 15:34:26 +0530

    Update 06 count_cycles_of_length_n.cpp

--- cycle_detection/06 count_cycles_of_length_n.cpp
+++ cycle_detection/06 count_cycles_of_length_n.cpp
@@ -7,53 +7,41 @@
 // Using DFS we find every possible path of length (n-1) for a particular source (or starting point). 
 // Then we check if this path ends with the vertex it started with, if yes then we count this as the cycle of length n.
 
-// CPP Program to count cycles of length n
-// in a given graph.
-#include <bits/stdc++.h>
-using namespace std;
+
+
 
 // Number of vertices
 const int V = 5;
 
-void DFS(bool graph[][V], bool marked[], int n, int vert,
-		int start, int& count)
+void DFS(bool graph[][V], bool marked[], int n, int vert, int start, int& count)
 {
 	// mark the vertex vert as visited
 	marked[vert] = true;
 
 	// if the path of length (n-1) is found
 	if (n == 0) {
-
-		// mark vert as un-visited to make
-		// it usable again.
+		// mark vert as un-visited to make it usable again.
 		marked[vert] = false;
 
-		// Check if vertex vert can end with
-		// vertex start
-		if (graph[vert][start] && graph[start][vert]) {
+		// Check if vertex vert can end with vertex start
+		if (graph[vert][start] && graph[start][vert]) 
 			count++;
-			return;
-		}
-		else
-			return;
+		
+		return;
 	}
 
-	// For searching every possible path of
-	// length (n-1)
-	for (int i = 0; i < V; i++)
+	// For searching every possible path of length (n-1)
+	for (int i = 0; i < V; i++){
 		if (!marked[i] && graph[vert][i])
-
-			// DFS for searching path by decreasing
-			// length by 1
+			// DFS for searching path by decreasing length by 1
 			DFS(graph, marked, n - 1, i, start, count);
+	}
 
-	// marking vert as unvisited to make it
-	// usable again.
+	// marking vert as unvisited to make it usable again.
 	marked[vert] = false;
 }
 
-// Counts cycles of length N in an undirected
-// and connected graph.
+// Counts cycles of length N in an undirected and connected graph.
 int countCycles(bool graph[][V], int n)
 {
 	// all vertex are marked un-visited initially.
@@ -64,24 +52,21 @@ int countCycles(bool graph[][V], int n)
 	int count = 0;
 	for (int i = 0; i < V - (n - 1); i++) {
 		DFS(graph, marked, n - 1, i, i, count);
-
-		// ith vertex is marked as visited and
-		// will not be visited again.
+		// ith vertex is marked as visited and will not be visited again.
 		marked[i] = true;
 	}
 
-	return count / 2;
+	return count / 2;  //
 }
 
 int main()
 {
 	bool graph[][V] = { { 0, 1, 0, 1, 0 },
-						{ 1, 0, 1, 0, 1 },
-						{ 0, 1, 0, 1, 0 },
-						{ 1, 0, 1, 0, 1 },
-						{ 0, 1, 0, 1, 0 } };
+				{ 1, 0, 1, 0, 1 },
+				{ 0, 1, 0, 1, 0 },
+				{ 1, 0, 1, 0, 1 },
+				{ 0, 1, 0, 1, 0 } };
 	int n = 4;
-	cout << "Total cycles of length " << n << " are "
-		<< countCycles(graph, n);
+	cout << "Total cycles of length " << n << " are "<< countCycles(graph, n);
 	return 0;
 }
