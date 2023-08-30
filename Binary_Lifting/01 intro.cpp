8929f88a211ffc55abce5a67cecfe32c73fe0985
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-08-30 16:14:35 +0530

    Update 01 intro.cpp

--- Binary_Lifting/01 intro.cpp
+++ Binary_Lifting/01 intro.cpp
@@ -3,7 +3,7 @@
 2. LCA in a tree
 
 MIS-CONECPTION: binary lifting ONLY works on TREES => WRONG!!!
-CORRECT: As long as the path to any node is unique, the binary lifting can be used. (eg a cyclic graph where each node has only 1 parent)
+CORRECT: As long as the path to any node is unique, binary lifting can be used. (eg a cyclic graph where each node has only 1 parent)
 
 pre-processing time : O(N*logN)
 query time : O(logN)
