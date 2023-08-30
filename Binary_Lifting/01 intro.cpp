e6262f81c88c62b7c4e108d832bf142de250f3da
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-08-30 16:13:40 +0530

    Update 01 intro.cpp

--- Binary_Lifting/01 intro.cpp
+++ Binary_Lifting/01 intro.cpp
@@ -2,5 +2,8 @@
 1. find Kth parent/ancestor of a tree node (NOTE: Tree is IMP)
 2. LCA in a tree
 
+MIS-CONECPTION: binary lifting ONLY works on TREES => WRONG!!!
+CORRECT: As long as the paths to a node are unique, the binary lifting can be used. (eg a cyclic graph where each node has only 1 parent)
+
 pre-processing time : O(N*logN)
 query time : O(logN)
