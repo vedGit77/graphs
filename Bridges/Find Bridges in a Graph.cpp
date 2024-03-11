f4dd04b960decfc6147600d5eebb951aafe56de9
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2024-03-11 15:28:02 +0530

    Update Find Bridges in a Graph.cpp

--- Bridges/Find Bridges in a Graph.cpp
+++ Bridges/Find Bridges in a Graph.cpp
@@ -14,118 +14,68 @@
 
 
 // Efficient solution:
-// A O(V+E) algorithm to find all Bridges 
-// The idea is similar to O(V+E) algorithm for Articulation Points. 
-// We do DFS traversal of the given graph. In DFS tree an edge (u, v) (u is parent of v in DFS tree) is bridge 
-// if there does not exist any other alternative to reach u or an ancestor of u from subtree rooted with v. 
-
-// Discovery time (disc): Dring DFS => time when a node is visited => disc time. 
-// low[v] indicates earliest visited vertex reachable from SUBTREE rooted with v. 
-// The condition for an edge (u, v) to be a bridge is, “low[v] > disc[u]”. 
+// O(V+E) time
+// Time of insertion (tin): Dring DFS => time when a node is visited
+// Lowest time (low): current node goes to all its adjacent nodes EXCEPT the parent and takes the MIN LOWEST TIME.
 
 
-//DIFF BW articulation pts and bridges ka code => 1) No children variable needed here
-//						  2) Condn for bridge / articulation 
+#include <bits/stdc++.h>
+using namespace std;
 
+class Solution {
+private:
+    int timer = 1;  //global
 
+    void dfs(int node, int parent, vector<int> &vis,  vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        for (auto it : adj[node]) {
+            if (it == parent)  //ignore parent
+		    continue;
+            if (vis[it] == 0){  //adj node not visited
+                dfs(it, node, vis, adj, tin, low, bridges);
+                low[node] = min(low[it], low[node]);  //min from low array
+                
+	        if (low[it] > tin[node])  //condn for bridge
+                    bridges.push_back({it, node});
+            }else{
+                low[node] = min(low[node], low[it]);   //only update low (if already visited)
+            }
+        }
+    }
+public:
+    vector<vector<int>> criticalConnections(int n,
+    vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for (auto it : connections) {
+            int u = it[0], v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<vector<int>> bridges;
+        dfs(0, -1, vis, adj, tin, low, bridges);
+        return bridges;
+    }
+};
 
-#include<iostream> 
-#include <list> 
-#define NIL -1 
-using namespace std; 
+int main() {
 
-class Graph 
-{ 
-	int V; // No. of vertices 
-	list<int> *adj; // A dynamic array of adjacency lists 
-	void bridgeUtil(int v, bool visited[], int disc[], int low[], 
-					int parent[]); 
-public: 
-	Graph(int V); // Constructor 
-	void addEdge(int v, int w); // to add an edge to graph 
-	void bridge(); // prints all bridges 
-}; 
+    int n = 4;
+    vector<vector<int>> connections = {
+        {0, 1}, {1, 2},
+        {2, 0}, {1, 3}
+    };
 
-Graph::Graph(int V) 
-{ 
-	this->V = V; 
-	adj = new list<int>[V]; 
-} 
+    Solution obj;
+    vector<vector<int>> bridges = obj.criticalConnections(n, connections);
+    for (auto it : bridges) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
+    return 0;
+}
 
-void Graph::addEdge(int v, int w) 
-{ 
-	adj[v].push_back(w); 
-	adj[w].push_back(v); // Note: the graph is undirected 
-} 
-
-
-void Graph::bridgeUtil(int u, bool visited[], int disc[], int low[], int parent[]) //ALL ARE PASSED BY REFERENCE => just like articulation points
-{ 
-	static int time = 0; //instead of static int, we could simply do => void bridgeUtil(..., int & time, ...) =>just like articulation points
-
-	visited[u] = true; 
-
-	disc[u] = low[u] = ++time; 
-	
-	//NO NEED of children variable here (was needed in articulation pts)
-
-	list<int>::iterator i; 
-	for (i = adj[u].begin(); i != adj[u].end(); ++i) 
-	{ 
-		int v = *i; 
-
-		if (!visited[v]) 
-		{ 
-			parent[v] = u; 
-			
-			//No children++ here like articulation points
-			
-			bridgeUtil(v, visited, disc, low, parent); //recursion 
-
-			low[u] = min(low[u], low[v]); 
-
-			if (low[v] > disc[u])  //NOT >=
-				cout << u <<" " << v << endl; 
-		} 
-
-		else if (v != parent[u])      
-			low[u] = min(low[u], disc[v]);
-	} 
-} 
-
-void Graph::bridge() //VVIMP....we have to make 2 functions.....1 se kaam NAHI hoga!!!...VVIMP....FOR BOTH BRIDGE AND ARTICULATION PT.
-{ 
-	// Mark all the vertices as not visited 
-	bool *visited = new bool[V]; 
-	int *disc = new int[V]; 
-	int *low = new int[V]; 
-	int *parent = new int[V]; 
-
-	// Initialize parent and visited arrays 
-	for (int i = 0; i < V; i++) 
-	{ 
-		parent[i] = NIL; 
-		visited[i] = false; 
-	} 
-
-	// Call the recursive helper function to find Bridges 
-	// in DFS tree rooted with vertex 'i' 
-	for (int i = 0; i < V; i++) 
-		if (visited[i] == false) 
-			bridgeUtil(i, visited, disc, low, parent); 
-} 
-
-
-int main() 
-{ 
-	cout << "Bridges in first graph \n"; 
-	Graph g(5); 
-	g.addEdge(1, 0); 
-	g.addEdge(0, 2); 
-	g.addEdge(2, 1); 
-	g.addEdge(0, 3); 
-	g.addEdge(3, 4); 
-	g.bridge(); 
-
-	return 0; 
-} 
